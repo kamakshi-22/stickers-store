9403a69714ef832f28e73432b8b1bd368fa21021
Kamakshi Karakoti <kamakshikarakoti22@gmail.com>
2023-02-16 16:31:45 +0530

    deploy

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,13 @@ import { ShoppingCart } from 'phosphor-react'
 import './Navbar.scss'
 import { ShopContext } from '../../context/ShopContext';
 
-
 export function Navbar() {
     const { cartItems } = React.useContext(ShopContext);
     const totalAmount = Object.values(cartItems).reduce((acc, curr) => acc + curr, 0)
     return (
         <div className='navbar'>
             <div className="links">
-                <Link to='/stickers-store'>Shop</Link>
+                <Link to='/'>Shop</Link>
                 <Link to='/cart' className='cart'>
                     <ShoppingCart size={32} className="cartIcon" />
                     <p className='.cartCount'>{totalAmount}</p>
